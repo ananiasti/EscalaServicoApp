208d7786f0ee475072cbc6cbbd176e9306628a22
Ananias <ananiasti@gmail.com>
2025-10-02 10:24:36 -0300

    Cadasrto de usuários pronto e imagem na tela de início

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,30 @@
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 type Usuarios = { id: number; nome: string };
 
 export default function Home() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       {/* Conteúdo central */}
+      <View style={styles.content}>
+        <Text style={styles.title}>Bem-vindo!</Text>
+        <Text style={styles.title}>Escala de Serviço da Paróquia São José Operário</Text>
 
+        <TouchableOpacity
+          onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+          activeOpacity={0.7}
+        >
+          <Image
+            source={require('../assets/images/SaoJoseOperario.png')}
+            style={styles.imginicio}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </View>
 
       {/* Rodapé */}
       <View style={styles.footer}>
@@ -20,9 +37,8 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: '#fff' },
   content: { flex: 1, alignItems: 'center', justifyContent: 'center', gap: 12, padding: 16 },
-  title: { fontSize: 20, fontWeight: '600' },
-  row: { flexDirection: 'row', gap: 8, alignItems: 'center', alignSelf: 'stretch' },
-  input: { flex: 1, borderWidth: 1, borderColor: '#ccc', borderRadius: 8, padding: 10 },
-  footer: { position: 'absolute', bottom: 0, left: 0, right: 0, padding: 16, alignItems: 'center',
-            backgroundColor: '#fff', }, footerText: { fontSize: 14, color: '#666' },
+  imginicio: { width: 650, height: 650, marginBottom: 16 },
+  title: { fontSize: 20, fontWeight: '600', textAlign: 'center' },
+  footer: { position: 'absolute', bottom: 0, left: 0, right: 0, padding: 16, alignItems: 'center', backgroundColor: '#fff' },
+  footerText: { fontSize: 14, color: '#666' },
 });
